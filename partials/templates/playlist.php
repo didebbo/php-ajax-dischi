23da049d7403cf9d75dc824190ae4f2c69870bff
didebbo <gianluca.napoletano.93@gmail.com>
2021-10-21 16:22:42 +0200

    refactoring

--- partials/templates/playlist.php
+++ partials/templates/playlist.php
@@ -1,24 +1,25 @@
 <div class="playlist">
-    <!-- <?= var_dump($database) ?> -->
     <div class="row">
         <?php foreach ($database as $item) : ?>
             <div class="col">
-                <div class="item">
-                    <div class="poster">
-                        <img src="<?= $item["poster"]; ?>" alt="">
-                    </div>
-                    <div class="title">
-                        <?= $item["title"]; ?>
-                    </div>
-                    <div class="desc">
-                        <div class="author">
-                            <?= $item["author"]; ?>
+                <a href="#">
+                    <div class="item">
+                        <div class="poster">
+                            <img src="<?= $item["poster"]; ?>" alt="">
                         </div>
-                        <div class="year">
-                            <?= $item["year"]; ?>
+                        <div class="title">
+                            <?= $item["title"]; ?>
+                        </div>
+                        <div class="desc">
+                            <div class="author">
+                                <?= $item["author"]; ?>
+                            </div>
+                            <div class="year">
+                                <?= $item["year"]; ?>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
         <?php endforeach ?>
     </div>
